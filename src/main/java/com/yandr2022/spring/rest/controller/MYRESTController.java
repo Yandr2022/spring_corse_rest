604e6ee5f8e8c5b2a79de5c7c4b0ef376a6e0a4b
Yandr2022 <sharkievich@gmail.com>
2023-03-18 07:00:54 +0300

    added Controller with logic:  show List, get, add new, update, delete Employees

---
+++ src/main/java/com/yandr2022/spring/rest/controller/MYRESTController.java
@@ -0,0 +1,53 @@
+package com.yandr2022.spring.rest.controller;
+
+import com.yandr2022.spring.rest.entity.Employee;
+import com.yandr2022.spring.rest.exception_handling.NoSuchEmployeeException;
+import com.yandr2022.spring.rest.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MYRESTController {
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " to DB");
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        if (employeeService.getEmployee(id) == null) {
+            throw new NoSuchEmployeeException("There is no employee with ID = " + id + " to DB");
+        }
+        employeeService.deleteEmployee(id);
+        return "Employee with id = " + id + " was deleted";
+    }
+
+}
