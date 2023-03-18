ee02252c917efa15024b5b0972a01ed081774b68
Yandr2022 <sharkievich@gmail.com>
2023-03-18 06:58:30 +0300

    added Exception Handling

---
+++ src/main/java/com/yandr2022/spring/rest/exception_handling/NoSuchEmployeeException.java
@@ -0,0 +1,7 @@
+package com.yandr2022.spring.rest.exception_handling;
+
+public class NoSuchEmployeeException extends RuntimeException {
+    public NoSuchEmployeeException(String message) {
+        super(message);
+    }
+}
